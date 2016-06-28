bc79314f278533d556746b9678adf0a5318f4ef9
linpin <lindalin07@gmail.com>
2016-06-28 14:05:58 +0800

    刪除

--- test.php
+++ test.php
@@ -13,26 +13,7 @@ and open the template in the editor.
         <?php
         class Cat
         {
-            public $name;//屬性
-            protected $position;
-            
-            public function __construct($name) //方法,建構子(未必要有)
-                    {
-                     $this->name = $name;
-                     $this->position = ['x'=>0,'y'=>0];
-                    }
-                    public function moveTo($x,$y)
-                    {
-                        $this->position['x'] = $x;
-                        $this->position['y'] = $y;
-                        return $this->position;
-                    }
-                    public function resetPosition()
-                    {
-                        $this->moveTo(0, 0);
-                        return $this->position;
-                                
-                    }
+             
         }
         
         $pet = new Cat("Kitty");
